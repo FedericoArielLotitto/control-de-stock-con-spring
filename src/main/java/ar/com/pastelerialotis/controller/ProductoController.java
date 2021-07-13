6a50631c1f9b17731477700ec543bedbc71c3cc2
Federico Ariel Lotitto <>
2021-07-12 22:18:09 -0300

    Agrega homepage y arregla maqueta de productos

--- src/main/java/ar/com/pastelerialotis/controller/ProductoController.java
+++ src/main/java/ar/com/pastelerialotis/controller/ProductoController.java
@@ -1,5 +1,29 @@
 package ar.com.pastelerialotis.controller;
 
+import ar.com.pastelerialotis.dominio.Producto;
+import ar.com.pastelerialotis.service.ProductoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class ProductoController {
     
+    @Autowired
+    ProductoService productoService;
+    
+    @GetMapping
+    public String home() {
+        return "home-page";
+    }
+    
+    @GetMapping("/productos")
+    public ModelAndView presentarProductos() {
+        ModelAndView productos = new ModelAndView("productos");
+        List<Producto> listaProductos = productoService.buscarProductosSegunMasVencido();
+        productos.addObject("productos", listaProductos);
+        return productos;
+    }
 }
